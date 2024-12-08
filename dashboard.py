51b454fc9b53d7496e2d16a011c5b0dc02c96b1a
exety <orangedonelle@soscandia.org>
2024-12-08 17:47:08 +1000

    Добавление файлов

---
+++ dashboard.py
@@ -0,0 +1,35 @@
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import plotly.express as px
+
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/IvanMA4HEB/TGBOTPROJECT/main/data.csv')
+
+# Initialize the app
+app = Dash(__name__)
+
+
+# App layout
+app.layout = html.Div([
+    html.Div(children='Выступления'),
+    html.Hr(),
+    dcc.RadioItems(options=['Время начала', 'Вместимость площадки'], value='Время начала', id='controls-and-radio-item'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+    dcc.Graph(figure={}, id='controls-and-graph'),
+    dcc.Graph(figure=px.pie(df, values='Номер', names='Тип выступления')),
+    dcc.Graph(figure=px.bar(df, x='Площадка', y='Вместимость площадки'))
+])
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-item', component_property='value')
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='Номер', y=col_chosen, histfunc='avg')
+    return fig
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,49 @@
+from multiprocessing.connection import Client
+
+import telebot
+from aiohttp.web_fileresponse import content_type
+from telebot import types
+from aiogram.types import ReplyKeyboardMarkup, keyboard_button
+bot = telebot.TeleBot('8195521689:AAErJkC-mgOTDICqXABkgJD45V3Phy9jxYE')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup_inline = types.InlineKeyboardMarkup()
+    item_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
+    item_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
+
+    markup_inline.add(item_yes, item_no)
+    bot.send_message(message.chat.id, 'Привет, это фотостудия Photolab, желаете узнать небольшую информацию про нас?', reply_markup=markup_inline)
+
+@bot.callback_query_handler(func=lambda call: True)
+def answer(call):
+    if call.data == 'yes':
+        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item_contacts = types.KeyboardButton('Наши контакты')
+        item_analytics = types.KeyboardButton('Наша Аналитика')
+        item_reservation = types.KeyboardButton('Бронирование')
+        item_information = types.KeyboardButton('О нас')
+        markup_reply.add(item_contacts, item_analytics, item_reservation , item_information)
+        bot.send_message(call.message.chat.id, 'Нажмите одну из кнопок', reply_markup=markup_reply)
+    elif call.data == 'no':
+        pass
+        bot.send_message(call.message.chat.id, 'Хорошо, если у вас будут вопросы, просто напишите!')
+
+@bot.message_handler(content_types=['text'])
+def get_text(message):
+    if message.text == 'Наши контакты':
+        bot.send_message(message.chat.id, 'Вот, держи наши контакты: +7 (914) 444-77-77 Почта: orangedonelle@soscandia.org Мы находимся по адресу')
+    elif message.text == 'Аналитика студии':
+        bot.send_message(message.chat.id, 'Наша Аналитика')
+    elif message.text == 'О нас':
+        bot.send_message(message.chat.id, 'Мы студия *PhotoLab* мы на рынке более 5-ти лет.')
+    elif message.text == 'Бронирование':
+        bot.send_message(message.chat.id,'Напишите дату, которое хотите забронировать')
+    elif message.text == 'date' or 'month' or 'year':
+        bot.send_message(message.from_user.id, 'Вы были записаны на фотостудию. Хорошего вам дня!')
+
+
+
+bot.polling(none_stop=True, interval=0)
+
+
